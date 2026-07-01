afe44ddf79f983593b70daf9e5c5218cc9f4f994
Hriday Adhikari <146981930+hridayadhikari@users.noreply.github.com>
2026-07-01 23:05:21 +0530

    Force HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
     Paginator::useTailwind();
-
+ if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
     }
 }
